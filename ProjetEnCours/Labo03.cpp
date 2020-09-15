0614e8bbada8590be642809581f68063adc30007
Karine Moreau <kmoreau@cstj.qc.ca>
2020-09-15 17:45:11 -0400

    Fin du cours 2020-09-15

--- ProjetEnCours/Labo03.cpp
+++ ProjetEnCours/Labo03.cpp
@@ -1,49 +1,42 @@
-// But : Les instructions conditionnelles 
-// Si le nombre est plus grand que 10 et  qu'il est plus petit que 20 alors c'est gagné, sinon, si c'est 20 alors droit à une deuxième chance, sinon c'est perdu
+// But : utiliser les instructions conditionnelles
+// Exemple : Si le nombre est 10 alors c'est gagné, sinon c'Est c'est 20 alors deuxième chance,
+//           sinon c'est perdu
 // Auteur : Karine Moreau
 // Date : 2020-09-15
-
 
 #include <iostream>
 
 int main()
 {
 	setlocale(LC_ALL, "");
-	// Déclaration des variables
-	int nombre = 20;				// Le nombre est initialisé grâce à l'opérateur d'affectation
+	// Déclarer une variable et on l'initialise avec l'opérateur = d'affectation
+	int nombre = 25;
 
-	// Instructions conditionnelles  => if (condition avec un opérateur de comparaison, == != < <= > >=)
-	// && (et) ou || (ou) pour lier deux conditions entre elles pour n'en former qu'une seule
-	// if (nombre > 10 && <20)
-	// if (10 < nombre < 20)		: incorrect car 2 opérateurs de comparaison sans && ||
-	if (nombre > 10 && nombre < 20)
+	// Instructions conditionnelles : if ... else en utilisant des opérateurs de comparaison (< > <= >= == !=)
+	if (nombre == 10)
 	{
-			std::cout << "Vous avez gagné.";
-			std::cout << "Félicitations !";
+		std::cout << "Vous avez gagné !";
+		std::cout << "Félicitations";
 	}
-				
-	
 	else
 		if (nombre == 20)
-		{
-			std::cout << "Vous avez le droit à une deuxième chance.";
-			std::cout << "Vous pouvez recommencer";
-		}
+			std::cout << "Vous avez droit à une seconde chance.";
 		else
-			std::cout << "vous avez perdu.";
-		
+			std::cout << "Vous avez perdu.";
+
+
 	return 0;
 }
 
-/* 
+/*
 Plan de tests
-
-5					perdu !
-10					perdu !
-15					gagné !
-20					deuxième chance !
-25					perdu !
-0					perdu !
--10					perdu !
+Nombre							Affichage à l'écran
+5								perdu
+10								gagné !
+15								perdu
+20								seconde chance
+25								perdu
+0								perdu
+-5								perdu
 
 */
---
+++ ProjetEnCours/feuilleErreurPasCommettre.cpp
@@ -0,0 +1,14 @@
+/*
+avec un if ne PAS METTRE de ; après la condition
+	if (nombre == 10);
+
+si on ne met pas d'accolade, seulement l'instruction suivante le if est prise en compte
+
+Si le else est souligné en rouge, vérifier pas de ; après les () du if
+
+
+LNK main() déjà existant : Vérifier si pas deux fichiers avec un main chacun dans le même projet
+
+
+
+*/
