78c753940b2a75d3806c0cce915a840559b80fe2
Karine Moreau <kmoreau@cstj.qc.ca>
2020-11-24 17:17:02 -0500

    Fin du cours du 2020-11-24

--- ProjetEnCours/Labo10Fichier.cpp
+++ ProjetEnCours/Labo10Fichier.cpp
@@ -4,6 +4,9 @@
 
 
 #include <iostream>
+#include <iomanip>				// Bibliothèque qui fournit des fonctions pour formater l'affichage
+								// (alignement (à droite, à gauche), préciser le nombre de chiffres après la virgule, 
+								// choisir le caractère de remplissage, ...)
 #include <fstream>				// Bibliothèque pour utiliser les fichiers dans le programme
 
 using namespace std;			// Pour éviter d'écrire std:: dans les instructions comme cout, cin, endl, ...			
@@ -13,6 +16,25 @@ int main()
 	setlocale(LC_ALL, "");
 
 	// Déclaration des constantes
+	/*
+----------------------------------------------------------------------------------
+				  Résultats du cours de programmation structurée
+----------------------------------------------------------------------------------
+Nom             Prénom              Eval 1    Eval 2    Eval 3     Total Résultats
+----------------------------------------------------------------------------------
+	*/
+	const int COL1 = 20;
+	const int COL2 = 20;
+	const int COL3 = 10;
+	const int COL4 = 10;
+	const int COL5 = 10;
+	const int COL6 = 10;
+	const int COL7 = 10;
+
+	const int LIGNE = COL1 + COL2 + COL3 + COL4 + COL5 + COL6 + COL7;
+	const string TITRE = "Résultats du cours de programmation structurée";
+
+
     // But : On veut ouvrir un fichier pour y lire des informations	
 	// Première phase : Création d'un canal pour extraire les informations sur le disque dur et les charger en mémoire
 
@@ -26,18 +48,18 @@ int main()
 	// Pour chaque champ (colonne) du fichier de données, il faut créer une variable
 	string nomEtudiant;
 	string prenomEtudiant;
-	int noteEval1;
-	int noteEval2;
-	int noteEval3;
+	float noteEval1;
+	float noteEval2;
+	float noteEval3;
 	// 2. pour les résultats à afficher ou à calculer. Il faut penser à initialiser ces variables au départ
 	int nbEtudiant = 0;			// Au départ, aucun étudiant n'a été lu dans le fichier. 
-	int noteMax;				// On n'a pas encore de max et on ne peut pas mettre 0, car s'il y avait que des nombres négatifs
+	float noteMax;				// On n'a pas encore de max et on ne peut pas mettre 0, car s'il y avait que des nombres négatifs
 								// et bien 0 ne serait pas le max. 
 	string prenomMax;			// Au départ, on ne connait pas le prénom de l'étudiant qui obtiendra le max
-	int noteMin;				
+	float noteMin;				
 	float moyenne = 0;			// Au départ, pas de note donc la moyenne est à 0
-	int noteFinale;				// Il faut avoir lu au moins 3 notes pour déterminer la note finale
-
+	float noteFinale;				// Il faut avoir lu au moins 3 notes pour déterminer la note finale
+	string resultat;
 
 
 
@@ -52,7 +74,8 @@ int main()
 	// Elle prend deux paramètres : 1. le nom du fichier sur le disque dur et au besoin le chemin.
 	//                            : 2. le mode d'ouverture du fichier : input pour en lecture
 	ifEntree.open(FICHIER_SOURCE, ios::in);
-	// 3. Lier le canal de sortie à sa destination
+	// 3. Lier le canal de sortie à sa destination : 
+	// le mode ios::out : permet de créer le fichier et ensuite de l'ouvrir. ATTENTION ios::out écrase le fichier s'il existe déjà
 	ofSortie.open(FICHIER_RESULTAT, ios::out);
 
 	//4. Avant de poursuivre le programme, il faut s'assurer que le canal a bien été ouvert sur le fichier, sinon on quitte 
@@ -70,10 +93,44 @@ int main()
 		// Pour quitter abruptement le programme, il faut utiliser le exit ou abort
 		//abort();				// Quitte sans code d'erreur
 		exit(400);				// Quitte avec un code d'erreur
-
 	}
 
+	if (!ofSortie)
+	{
+		cerr << "Erreur : Le fichier " << FICHIER_RESULTAT << " n'a pas été créé." << endl;
+		cerr << "Vérifiez l'espace disponible sur le disque dur." << endl;
+		cerr << "Ou assurez-vous d'avoir les autorisations nécessaires pour créer le fichier." << endl;
+		system("pause");
+		exit(911);
+	}
+
+
+
 	// Ici tout va pour le mieux !!! 
+	// On a maintenant deux nouveaux canaux, un pour lire les informations, ifEntree qui substituera le cin
+	// un autre ofSortie qui remplacera le cout, au lieu d'afficher à l'écran on écrit dans un fichier.
+
+	// Comme le fichier Résultat.txt vient d'être créé, on peut tout de suite écrire l'en-tête du fichier
+	/*
+----------------------------------------------------------------------------------
+				  Résultats du cours de programmation structurée
+----------------------------------------------------------------------------------
+Nom             Prénom              Eval 1    Eval 2    Eval 3     Total Résultats
+----------------------------------------------------------------------------------
+	*/
+	// set permet de définir, w pour width, la largeur d'une colonne et l'information qui suit juste après le setw, sera contenu
+	// dans cette colonne.
+	ofSortie << left << setfill('-') << setw(LIGNE) << "-" << right << setfill(' ') << endl;
+	ofSortie << setw((LIGNE - TITRE.length()) / 2) << " " << TITRE << endl;
+	ofSortie << left << setfill('-') << setw(LIGNE) << "-" <<  right << setfill(' ')<< endl;
+	ofSortie << left << setw(COL1) << "Nom" << setw(COL2) << "Prénom" << right << setw(COL3) << "Eval 1" << setw(COL4) << "Eval 2";
+	ofSortie << setw(COL5) << "Eval 3" << setw(COL6) << "Total" << left << setw(COL7) << " Résultats" << right << endl;
+	ofSortie << left << setfill('-') << setw(LIGNE) << "-" << right << setfill(' ') << endl;
+
+
+
+
+
 
 	// Deuxième phase : Lire les informations dans le fichier
 	// ON TENTE lire un enregistrement en lisant les 5 champs à la fois pour forcer le canal à mettre à jour s'il a atteint ou 
@@ -104,21 +161,25 @@ int main()
 
 		// On peut faire un traitement sur les données
 
-		cout << "Nom : " << nomEtudiant << endl;
-		cout << "Prenom : " << prenomEtudiant << endl;
-		cout << "Note 1 : " << noteEval1 << endl;
-		cout << "Note 2 : " << noteEval2 << endl;
-		cout << "Note 3 : " << noteEval3 << endl;
 		noteFinale = noteEval1 + noteEval2 + noteEval3;
-		cout << "Note finale : " << noteFinale << endl;
 		if (noteFinale >= 60)
 		{
-			cout << "Vous passez le cours !!!!" << endl;
+			resultat = " Succès";
 		}
 		else
 		{
-			cout << "Malgré vos efforts, il faudra reprendre le cours. " << endl;
+			resultat = " Echec";
 		}
+		// On veut mettre deux chiffres après la virgule pour les nombres réels
+		// Première chose, il faut que la virgule des nombres flottant soit fixe, qu'elle ne bouge plus
+		// Alors utiliser setprecision(2) pour mettre 2 chiffres après la virgule
+		ofSortie << fixed << setprecision(2);
+		ofSortie << left << setw(COL1) << nomEtudiant << setw(COL2) << prenomEtudiant << right << setw(COL3) << noteEval1 << setw(COL4) << noteEval2;
+		ofSortie << setw(COL5) << noteEval3 << setw(COL6) << noteFinale << left << setw(COL7) << resultat << right << endl;
+
+
+
+
 		// On ajoute la note finale au total pour faire la moyenne
 		moyenne = moyenne + noteFinale;
 		nbEtudiant++;
@@ -148,10 +209,10 @@ int main()
 	if (nbEtudiant > 0)
 	{
 		moyenne = moyenne / nbEtudiant;
-		cout << "Le groupe a " << nbEtudiant << endl;
-		cout << "La note la plus haute est " << noteMax << " obtenue par " << prenomMax << endl;
-		cout << "La note la plus basse est " << noteMin << endl;
-		cout << "La moyenne du groupe est " << moyenne << endl;
+		ofSortie << "Le groupe a " << nbEtudiant << endl;
+		ofSortie << "La note la plus haute est " << noteMax << " obtenue par " << prenomMax << endl;
+		ofSortie << "La note la plus basse est " << noteMin << endl;
+		ofSortie << "La moyenne du groupe est " << moyenne << endl;
 	}
 	else
 		cout << "Le fichier est vide." << endl;
